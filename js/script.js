552e8715a2a4b5414a1bafaf295c90ef00b275c6
Andy Glover <andrew.l.glover@gmail.com>
2022-08-28 22:37:22 -0700

    Add constructor, function to add book, and some comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+let myLibrary = [];
+
+function Book(title) {
+    this.title = title;
+}
+
+function addBookToLibrary(userInput) {
+    new Book(userInput);
+}
+
+//define userinput from an input on the page.
+
+//loop through array of books and display them on the page
+//  add element for each book.
