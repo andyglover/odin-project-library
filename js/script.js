d3364badf80f439ea221debbaee6a54e3f8b2755
Andy Glover <andrew.l.glover@gmail.com>
2022-08-30 23:24:34 -0700

    Add trashcan svg to book delete button

--- js/script.js
+++ js/script.js
@@ -47,7 +47,10 @@ function loopThroughLibrary() {
         p.innerText = `Title: ${title}\nAuthor: ${author}\n${pages} Pages\n${read}`;
         //create delete button
         const deleteButton = document.createElement("button");
-        deleteButton.innerText="delete";
+        deleteButton.innerHTML=
+            `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+            </svg>`
         deleteButton.setAttribute("data-index", i);
         deleteButton.addEventListener('click',(e) => {
             myLibrary.splice(deleteButton.getAttribute("data-index"), 1);
